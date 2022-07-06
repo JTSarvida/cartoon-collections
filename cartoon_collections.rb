f097abc0e9cebec491a19f1460ec374009e3c411
jtsarvida <jtsarvida@gmail.com>
2022-07-05 23:22:53 -0400

    commit tree 56447374b867e9264af0020c3b5b22d81b2e1c0f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index {|dwarf, i| puts "#{i+1}. #{dwarf}"}
+
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(elements)# code an argument here
   # Your code here
+  newElement = []
+  elements.map {|element| newElement << element.capitalize + "!"}
+  newElement
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any?{|i| i.size > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect {|food| cheese_types.include?(food)}
 end
